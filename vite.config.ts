7056451b66b83d64b3d586697a23479432d66308
William Y. Feng <womogenes2@gmail.com>
2025-07-17 05:31:07 -0400

    Support custom certificate on local only

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-// import fs from 'fs';
+import fs from 'fs';
 
-export default defineConfig({
+import { NODE_ENV } from '$env/static/private';
+
+const config = {
   plugins: [tailwindcss(), sveltekit()],
   server: {
-    // https: {
-    //   key: fs.readFileSync('localhost-key.pem'),
-    //   cert: fs.readFileSync('localhost.pem'),
-    // },
     host: true,
   },
-});
+};
+
+if (NODE_ENV === 'development') {
+  config.server['https'] = {
+    key: fs.readFileSync('localhost-key.pem'),
+    cert: fs.readFileSync('localhost.pem'),
+  };
+}
+
+export default defineConfig(config);
