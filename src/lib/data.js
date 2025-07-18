ca1b8880e7fa441d479b61f35693ed6d190c181d
William Y. Feng <womogenes2@gmail.com>
2025-07-18 02:01:15 -0400

    Don't send data in dev

--- src/lib/data.js
+++ src/lib/data.js
@@ -39,7 +39,7 @@ export const deobfuscate = (b64encRotated) => {
 
 export const sendAccelerationHistory = async (accelerationHistory) => {
   // Don't send in dev
-  // if (PUBLIC_NODE_ENV === 'development') return;
+  if (PUBLIC_NODE_ENV === 'development') return;
 
   // Don't send null data (e.g. from desktops)
   if (!accelerationHistory || accelerationHistory.length === 0) return;
